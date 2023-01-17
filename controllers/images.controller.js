0d964a293391e183f9545af26b532d41d733bf9b
Eliot Valdes <eliottvaldes@hotmail.com>
2023-01-17 14:54:36 -0600

    upd -> create file upload of an image using cloudinary

--- controllers/images.controller.js
+++ controllers/images.controller.js
@@ -1,23 +1,37 @@
-const { request, response } = require("express")
+const { request, response } = require("express");
 
+// import cloudinary package
+const cloudinary = require('cloudinary').v2
+// configure cloudinary account
+cloudinary.config(process.env.CLOUDINARY_URL);
 
-const uploadImage = (req = request, res = response) => {
-    const { image } = req.body;
+const uploadImage = async (req = request, res = response) => {
+    const { fileUpload } = req.files;
 
-    console.log(req.body)    
-    console.log(req.params)    
-    console.log(req.query)    
-
-    if (!image) {
+    if (!fileUpload) {
         return res.status(400).json({
-            msg: 'No image found'
+            ok: false,
+            msg: 'No image file found'
         });
     }
 
-    res.status(201).json({
-        msg: 'PUT upload image',
-        image
-    });
+    try {
+        const { tempFilePath } = req.files.fileUpload;
+        const { secure_url } = await cloudinary.uploader.upload(tempFilePath);
+
+        return res.status(201).json({
+            ok: true,
+            msg: 'Image uploaded successfully',
+            url: secure_url
+        });
+
+    } catch (error) {
+        return res.status(400).json({
+            ok: false,
+            msg: 'There was an error while upload the image'
+        });
+    }
+
 }
 
 const analizaImage = (req = request, res = response) => {
