d3f24c72b966137088e6fb568ef60998d6863bf6
Eliot Valdes <eliottvaldes@hotmail.com>
2023-01-17 14:55:45 -0600

    upd -> implement 'ok' property in fileValidation response

--- middlewares/validateFile.js
+++ middlewares/validateFile.js
@@ -5,6 +5,7 @@ const validateFileToUpload = (req, res = response, next) => {
 
     if (!req.files || Object.keys(req.files).length === 0 || !req.files.fileUpload) {
         return res.status(400).json({
+            ok: false,
             msg: 'There are no files to upload'
         });
     }
