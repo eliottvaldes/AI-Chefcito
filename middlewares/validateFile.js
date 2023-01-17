111a610d5356678350649927ecaef2ed6f1f93b4
Eliot Valdes <eliottvaldes@hotmail.com>
2023-01-17 14:34:19 -0600

    upd -> change the req.files.* property to fileUpload instead of archivo

--- middlewares/validateFile.js
+++ middlewares/validateFile.js
@@ -3,7 +3,7 @@ const { response } = require('express');
 
 const validateFileToUpload = (req, res = response, next) => {
 
-    if (!req.files || Object.keys(req.files).length === 0 || !req.files.archivo) {
+    if (!req.files || Object.keys(req.files).length === 0 || !req.files.fileUpload) {
         return res.status(400).json({
             msg: 'There are no files to upload'
         });
