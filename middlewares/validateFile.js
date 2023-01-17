131b5b65f71e74462f267a7d03213511da549e6f
Eliot Valdes <eliottvaldes@hotmail.com>
2023-01-17 14:20:37 -0600

    add -> create middleware to validate the upload of any file

---
+++ middlewares/validateFile.js
@@ -0,0 +1,19 @@
+const { response } = require('express');
+
+
+const validateFileToUpload = (req, res = response, next) => {
+
+    if (!req.files || Object.keys(req.files).length === 0 || !req.files.archivo) {
+        return res.status(400).json({
+            msg: 'There are no files to upload'
+        });
+    }
+
+    next();
+
+}
+
+
+module.exports = {
+    validateFileToUpload
+}
