9220c9a58c838319c3d62a5b3ab834a1bb287130
Eliot Valdes <eliottvaldes@hotmail.com>
2023-01-17 17:25:18 -0600

    add -> initial setup of files that contains the logic to recipes

---
+++ routes/recipes.routes.js
@@ -0,0 +1,14 @@
+const { Router } = require('express');
+const { check } = require('express-validator');
+
+const { validateFields } = require('../middlewares/validateFields');
+
+const {} = require('../controllers/recipes.controller');
+
+const router = Router();
+
+// Define all recipes-routes
+
+
+
+module.exports = router;
