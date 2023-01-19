76ba91065921a3da3728ce90ecb18f09540bb745
Eliot Valdes <eliottvaldes@hotmail.com>
2023-01-19 03:17:25 -0600

    upd -> use helper to get recipes-openai

--- controllers/recipes.controller.js
+++ controllers/recipes.controller.js
@@ -1,15 +1,11 @@
 const { request, response } = require("express");
-const { Configuration, OpenAIApi } = require("openai");
-
-const configuration = new Configuration({
-    apiKey: process.env.OPENAI_API_KEY,
-});
-const openai = new OpenAIApi(configuration);
+const { getRecipeOpenAI } = require("../helpers/getRecipeOpenAi");
 
 
 const getRecipes = async (req = request, res = response) => {
 
     const { ingredients = [] } = req.body;
+
     if (!ingredients || ingredients.length === 0) {
         return res.status(400).json({
             ok: false,
@@ -17,43 +13,17 @@ const getRecipes = async (req = request, res = response) => {
         });
     }
 
-    if (!configuration.apiKey) {
-        return res.status(500).json({
-            error: {
-                message: "OpenAI API key not configured, please follow instructions in README.md",
-            }
-        });
-    }
 
-    let prompt = `Write a recipe strictly based on these ingredients and give the instructions:\nIngredients:\n-${ingredients.join('\n-')}`;
+   const recipe = await getRecipeOpenAI(ingredients);
 
-    try {
-        const recipe = await openai.createCompletion({
-            model: "text-davinci-003",
-            prompt,
-            temperature: 1,
-            max_tokens: 400,
-            top_p: 1.0,
-            frequency_penalty: 0.0,
-            presence_penalty: 0.0,
-        });
-
-        return res.status(200).json({
-            ok: true,
-            msg: 'Recipes retrieved successfully',
-            prompt,
-            result: recipe.data.choices[0].text
-        });
-
-
-    } catch (error) {
-        return res.status(500).json({
+    if(!recipe.ok){
+        return res.status(400).json({
             ok: false,
-            msg: 'Error while trying to get recipes',
-            error
+            msg: 'Error trying to get recipes from OpenAI',
         });
     }
 
+    return res.status(200).json(recipe);
 
 }
 
