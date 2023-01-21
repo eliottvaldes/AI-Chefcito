b5c44855b0de10c59c51de2a460ac0b28ca418d4
Eliot Valdes <eliottvaldes@hotmail.com>
2023-01-21 02:48:48 -0600

    upd-> modify the openAi parameters to get a recipe

--- helpers/getRecipeOpenAi.js
+++ helpers/getRecipeOpenAi.js
@@ -16,8 +16,8 @@ const getRecipeOpenAI = async (ingredients = [], cutomizations = {}) => {
             model: "text-davinci-003",
             prompt,
             temperature: 1,
-            max_tokens: 400,
-            top_p: 1.0,
+            max_tokens: 500,
+            top_p: 8.0,
             frequency_penalty: 0.0,
             presence_penalty: 0.0,
         });
