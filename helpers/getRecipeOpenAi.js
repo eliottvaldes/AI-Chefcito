a37946d94551a90134186aab3601290fa80b3ef2
Eliot Valdes <eliottvaldes@hotmail.com>
2024-01-11 07:26:58 -0600

    Add getIngredientsOpenAI function for image analysis

--- helpers/getRecipeOpenAi.js
+++ helpers/getRecipeOpenAi.js
@@ -8,6 +8,7 @@ const openai = new OpenAI({ apiKey: process.env.OPENAI_API_KEY });
 const apiRequest = async (model, systemContent, userContent) => {
     try {
         const completion = await openai.chat.completions.create({
+            model,
             messages: [
                 {
                     role: "system",
@@ -18,11 +19,15 @@ const apiRequest = async (model, systemContent, userContent) => {
                     content: userContent
                 }
             ],
-            model,
+            max_tokens: 600,
         });
+
+        console.log(completion.usage);
+        console.log(completion.choices[0]);
 
         return completion.choices[0].message.content ?? '';
     } catch (error) {
+        console.log(`Error in apiRequest(): ${error}`);
         return '';
     }
 }
@@ -54,6 +59,41 @@ const getRecipeOpenAI = async (ingredients = [], cutomizations = {}) => {
 }
 
 
+
+// Función para obtener los ingredientes de una imagen
+const getIngredientsOpenAI = async (imageUrl) => {
+    try {
+        //const base64Image = await encodeImage(imageUrl);
+        const userContent = [
+            {
+                'type': 'text',
+                'text': 'Identifica todos los alimentos que hay en la imagen. Genera una descripción de 5 palabras y un arreglo de los alimentos encontrados. responde estrictamente con un objeto que tenga la estructura: {"imgDescription": description, "foodFound": []}. No respondas nada más que el objeto pero en formato de texto plano.'
+            },
+            {
+                'type': 'image_url',
+                'image_url': {
+                    "url": imageUrl,
+                },
+            }
+        ];
+        const model = 'gpt-4-vision-preview';
+        const systemContent = 'Eres un valioso asistente especializado en analizar imagenes de comida.';
+        let ingredients = await apiRequest(model, systemContent, userContent);
+        if (!ingredients) {
+            return { ok: false };
+        }
+
+        return {
+            ok: true,
+            data: ingredients
+        };
+    } catch (error) {
+        console.error(error);
+        return { ok: false };
+    }
+};
+
 module.exports = {
-    getRecipeOpenAI
+    getRecipeOpenAI,
+    getIngredientsOpenAI,
 }
