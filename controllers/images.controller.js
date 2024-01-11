1e47ae9cfb5f95b767f524f1e3cbf96a593553bd
Eliot Valdes <eliottvaldes@hotmail.com>
2024-01-11 07:26:26 -0600

    Add analyzeImageOpenAI function

--- controllers/images.controller.js
+++ controllers/images.controller.js
@@ -5,6 +5,7 @@ cloudinary.config(process.env.CLOUDINARY_URL);
 const { azureAnalyzeImage } = require('../helpers/analyzeImageAzure');
 const { validateCategories, validateTags, validateAdultContent } = require('../helpers/validateAnalysis');
 const { getFoodObjects } = require("../helpers/getImageObjects");
+const { getIngredientsOpenAI } = require("../helpers/getRecipeOpenAi");
 
 
 const uploadImage = async (req = request, res = response) => {
@@ -75,7 +76,7 @@ const analizaImage = async (req = request, res = response) => {
             msg: 'The image does not contain food - objects'
         });
     }
-        
+
     const imgDescription = description.captions[0].text ?? 'No description found';
 
     res.status(200).json({
@@ -88,8 +89,55 @@ const analizaImage = async (req = request, res = response) => {
 
 }
 
+const analyzeImageOpenAI = async (req = request, res = response) => {
+
+    const { image } = req.body;
+
+    if (!image) {
+        return res.status(400).json({
+            ok: false,
+            msg: 'No image url found'
+        });
+    }
+
+    /* const response = await getIngredientsOpenAI(image);
+
+    if (!response.ok) {
+        return res.status(400).json({
+            ok: false,
+            msg: 'There was an error while image analysis'
+        });
+    } */
+
+    const response = {
+        ok: true,
+        msg: 'Recipes retrieved successfully',
+        data: '{"imgDescription": "Variety fresh colorful food", "foodFound": ["watermelon", "banana", "pineapple", "strawberries", "melon", "pomegranate", "plum", "lemon", "bread", "pasta", "chicken breast", "steak", "fish", "cheese", "ham", "milk", "blackberries", "tomato", "green bell pepper", "onion", "potato", "carrot", "broccoli", "red cabbage", "green beans", "red bell pepper"]}'
+    }    
+
+    const { imgDescription, foodFound } = parseJSONToObject(response.data);
+
+    res.status(200).json({
+        ok: true,
+        msg: 'Image analyzed successfully',
+        imgDescription,
+        foodFound,
+    });
+}
+
+
+const parseJSONToObject = (jsonString) => {
+    try {
+        const parsedObject = JSON.parse(jsonString);
+        return parsedObject;
+    } catch (error) {
+        console.error('Error al parsear JSON:', error);
+        return jsonString;
+    }
+}
 
 module.exports = {
     uploadImage,
-    analizaImage
+    analizaImage,
+    analyzeImageOpenAI
 }
